d8d625f48780034ad9a596983853f59badf73429
AureleSecG <aurele.guitard.pro@gmail.com>
2022-04-13 18:02:44 +0200

    Setup

---
+++ cryptoappdisplay/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Homepage from "./Pages/Homepage";
+import Coinpage from "./Pages/Coinpage";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} exact />
+          <Route path="/coins/:id" element={<Coinpage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
